f5e2f8ab200a6c272746aba23df726418e71b921
Hebert <hebertphp@gmail.com>
2022-03-13 08:34:31 -0300

    PFE_2a_M

---
+++ aula4/script4_02.js
@@ -0,0 +1,7 @@
+var a,b;
+a=prompt("Digite um número");
+a=parseInt(a);
+b=prompt("Digite outro número");
+b=parseInt(b);
+document.write ("soma: "+ (a+b));
+document.write ("<br>subtração: "+ (a-b));
