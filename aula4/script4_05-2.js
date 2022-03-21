71dc33220b895b4c4a4ee3e10a96021216197c9c
Hebert <hebertphp@gmail.com>
2022-03-21 11:28:19 -0300

    PFE_2a_M

--- aula4/script4_05-2.js
+++ aula4/script4_05-2.js
@@ -1,12 +1,19 @@
 var x;
 x=prompt("Digite um número");//5
 x=parseInt(x);
-x++;
-x++;
-++x;
-++x;
-document.write("x="+x);//
-x--;
-x--;
---x;
-document.write("<br>x="+x);//
+x++;//6
+x++;//7
+++x;//8
+++x;//9
+document.write("x="+x);//x=9
+x--;//8
+x--;//7
+--x;//6
+document.write("<br>x="+x);//x=6
+/*
+As operações abaixo fazem a mesma coisa
+somam 1
+x++;//pós-incremento
+x=x+1;//soma
+x+=1;//oper de atribuição
+*/
