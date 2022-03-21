59ec83bc4974af1aa9e02fbcb3af2f9c447d3d1a
Hebert <hebertphp@gmail.com>
2022-03-21 10:46:58 -0300

    PFE_2a_M

---
+++ aula4/script4_03-1.js
@@ -0,0 +1,14 @@
+var a,b;
+a=prompt("Digite um número");
+a=parseInt(a);//3
+b=prompt("Digite outro número");
+b=parseInt(b);//2
+a%=b;//a=1
+b-=5;//b=-3
+document.write ("a = " + a);//
+document.write ("<br>b = "+ b);//
+/*
+Saída:
+a = 
+b = 
+*/
---
+++ aula4/script4_04-1.js
@@ -0,0 +1,7 @@
+var a,b;//
+a=prompt("Digite o nome");//Belarmina
+b=prompt("Digite o sobrenome");//Pafuncia
+document.write (a +" "+ b);//Belarmina Pafuncia
+b+=" ";//Pafuncia 
+b+=a;//Pafuncia Belarmina
+document.write ("<br>"+ b);//Pafuncia Belarmina
