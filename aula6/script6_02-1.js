74829d35b5589b8013f3b369559b132981e904c8
Hebert <hebertphp@gmail.com>
2022-05-02 11:32:05 -0300

    PFE_2a_M

--- aula6/script6_02-1.js
+++ aula6/script6_02-1.js
@@ -1,11 +1,12 @@
 var x=[2020,2,"Hoje"];
-x[0]++;
-x[1]=x[0]%2;
-x[2]+=" em dia";
-
+x[0]++;//2021
+x[1]=x[0]%2;//1
+x[2]+=" em dia";//Hoje em dia
+document.querySelector("#dv1").innerHTML=x[0]+"<br>"+x[1];
+document.querySelector(".teste").innerHTML=x[2];
 /*
 x[0] e x[1] saída para o id="dv1"
-x[1] saída para a class="teste"
+x[2] saída para a class="teste"
 */
 //document.write(x[0]+"<br>");
 //document.write(x[1]+"<br>");
