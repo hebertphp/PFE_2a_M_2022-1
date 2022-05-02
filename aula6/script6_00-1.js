2c22b4749ef160e729c046e5fcd797a3215ca412
Hebert <hebertphp@gmail.com>
2022-05-02 11:05:59 -0300

    PFE_2a_M

--- aula6/script6_00-1.js
+++ aula6/script6_00-1.js
@@ -7,6 +7,9 @@ i=2;//2
 i=17;//17
 x=[10,37,58,10,2,17];
 //document.write(i+"<br>");
-document.getElementById("dv1").innerHTML=i;
+//document.getElementById("dv1").innerHTML=i;
+document.querySelector("#dv1").innerHTML=i;
 document.querySelector(".teste").innerHTML=x;
+document.querySelector("h1").innerHTML+=", Será que<br> funciona?";
+
 //document.write(x);
