37da40da20c058ae1aef2f94254231a2f1d5c9a8
Hebert <hebertphp@gmail.com>
2022-05-09 10:16:06 -0300

    PFE_2a_M

---
+++ aula6/script6_03-1.js
@@ -0,0 +1,19 @@
+var nota=[10,8.5,7.75,9.5];
+var num=[];
+num[0]=120;
+num[1]=200;
+num[2]=2020;
+var nome=new Array("Jose","Paula","Carla");
+document.querySelector("#dv1").innerHTML=nota+"<br>"+num;
+document.querySelector(".teste").innerHTML=nome;
+
+/*
+a saída da var nota e num -> #dv1
+a saída de nome -> .teste
+Mudar o conteúdo da tag H1 para -> Paula
+*/
+
+
+// document.write(nota+"<hr>");
+// document.write(num+"<hr>");
+// document.write(nome+"<hr>");
