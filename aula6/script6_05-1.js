2ea69805b1fed042e05ce4a2f716cb3fc622db1c
Hebert <hebertphp@gmail.com>
2022-05-16 09:38:57 -0300

    PFE_2a_M

--- aula6/script6_05-1.js
+++ aula6/script6_05-1.js
@@ -1,11 +1,14 @@
-var num=[10,20,30,40,50];
+var num=[10,20,30,40,50,1,2,4,5,6,7,8,6,5,4,3,3,1,12,3,41,2022];
 var tam;
 tam=num.length;
-
+document.querySelector("#dv1").innerHTML="var num possui "+tam+" valores<hr>"+"Todos os valores da var num: "+num+"<hr>";
+document.querySelector(".teste").innerHTML="Valor da primeira posição de num: "+num[0]+"<hr>"+"Valor da última posição de num: "+num[num.length-1]+"<hr>";
+document.querySelector("h2").style.backgroundColor="red";
 /*
 document.write("var num possui "+tam+" valores<hr>");
 document.write("Todos os valores da var num: "+num+"<hr>");
 document.write("Valor da primeira posição de num: "+num[0]+"<hr>");
 document.write("Valor da última posição de num: "+num[tam-1]+"<hr>");
 mudar de document.write para querySelector
+dois document.write em cada div
 */
---
+++ aula6/script6_06-1.js
@@ -0,0 +1,6 @@
+var uf=["SP","RJ","ES","MG"];
+var i;
+document.querySelector("#dv1").innerHTML=uf+"<br>"+uf.length;
+for (i=0;i<uf.length;i++){
+    document.write(uf[i]+"<br>");
+}
