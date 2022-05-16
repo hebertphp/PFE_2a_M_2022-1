2c40f2a5b2e654d921023a5560138616d3f41b10
Hebert <hebertphp@gmail.com>
2022-05-16 11:29:43 -0300

    PFE_2a_M

--- aula6/script6_06-2.js
+++ aula6/script6_06-2.js
@@ -1,10 +1,14 @@
 var i;
 for (i=128512;i<=128580;i++){
    /* document.write("&#"+i+" - "+i+"<br>"); comentado */
-   
+    document.querySelector("#tb1").innerHTML+="<tr><td>&#"+i+"</td><td>"+i+"</td></tr>";
 }
 
 /*
+        <tr>
+            <td>&#128512</td>
+            <td>128512</td>
+        </tr>
 Desafio 2
 Criar um looping para pegar e mostrar os emojis:
 Saída em tabela!
