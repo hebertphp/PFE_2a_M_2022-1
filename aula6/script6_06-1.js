e57920c3a8bd810149695a49bb9e76bd69bc7435
Hebert <hebertphp@gmail.com>
2022-05-16 10:30:30 -0300

    PFE_2a_M

--- aula6/script6_06-1.js
+++ aula6/script6_06-1.js
@@ -1,6 +1,19 @@
-var uf=["SP","RJ","ES","MG"];
+var emoji=["&#128562","&#9749","&#129409","&#9973","&#9965"];
 var i;
-document.querySelector("#dv1").innerHTML=uf+"<br>"+uf.length;
-for (i=0;i<uf.length;i++){
-    document.write(uf[i]+"<br>");
+document.querySelector("#dv1").innerHTML=emoji+"<br>"+emoji.length;
+for (i=0;i<emoji.length;i++){
+    document.querySelector("#emoji").innerHTML+="<option>"+emoji[i]+"</option>";
 }
+// document.querySelector("#emoji").innerHTML="<option>MT</option>";
+/*
+Desafio 1
+Criar os valores para caixa select option, buscando 
+os valores contidos no array emoji
+
+Desafio 2
+Criar um looping para pegar e mostrar os emojis:
+Saída em tabela!
+de: 128512
+até:128580
+
+*/
