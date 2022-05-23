2608c538cc74f11d686241bd38622450b4aed082
Hebert <hebertphp@gmail.com>
2022-05-23 10:01:41 -0300

    PFE_2a_M

---
+++ aula6/aula6_duvida1.js
@@ -0,0 +1,5 @@
+var uf=["SP","RJ","MG","ES","PR"];
+var imagens=["cach.jpg","flo.jpg"];
+document.querySelector("h2").innerHTML=uf.length;
+document.querySelector("img").src="img/"+imagens[0];
+document.querySelector("#dv1").innerHTML="<img src='img/flo.jpg'>";
