8a1cd9a8042549a847c6c77b7f0d30f79fe29270
Hebert <hebertphp@gmail.com>
2022-06-06 10:24:04 -0300

    PFE_2a_M

--- aula7/script7_02.js
+++ aula7/script7_02.js
@@ -1,5 +1,14 @@
 function boas_vindas(nome) {
     const saida=document.querySelector("#saida");
-    saida.innerHTML="Seja bem vindo <b>"+nome+"</b>!";
-}
-boas_vindas("Manuele");
+    saida.innerHTML+="Seja bem vindo(a) <b>"+nome+"</b>!<br>";
+}// criação da função
+
+function boas_vindas_input(){
+    const outrasaida=document.querySelector("#outrasaida");
+    const login=document.querySelector("#login");
+    const senha=document.querySelector("#senha");
+    outrasaida.innerHTML+="Seja bem vindo(a) <b>"+login.value+"</b>!<br>Senha: "+senha.value+"<br>";
+}// criação da função
+
+boas_vindas("Manuele");//chamada da função, execução da função
+boas_vindas("Anacleto");
