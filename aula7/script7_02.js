af397d48eecccfe2c646ffed9ee5042abd3fbd25
Hebert <hebertphp@gmail.com>
2022-06-06 10:31:03 -0300

    PFE_2a_M

--- aula7/script7_02.js
+++ aula7/script7_02.js
@@ -1,14 +1,20 @@
+const saida=document.querySelector("#saida");
+const outrasaida=document.querySelector("#outrasaida");
+const login=document.querySelector("#login");
+const senha=document.querySelector("#senha");
+
 function boas_vindas(nome) {
-    const saida=document.querySelector("#saida");
-    saida.innerHTML+="Seja bem vindo(a) <b>"+nome+"</b>!<br>";
+     saida.innerHTML+="Seja bem vindo(a) <b>"+nome+"</b>!<br>";
 }// criação da função
 
 function boas_vindas_input(){
-    const outrasaida=document.querySelector("#outrasaida");
-    const login=document.querySelector("#login");
-    const senha=document.querySelector("#senha");
-    outrasaida.innerHTML+="Seja bem vindo(a) <b>"+login.value+"</b>!<br>Senha: "+senha.value+"<br>";
+     outrasaida.innerHTML+="Seja bem vindo(a) <b>"+login.value+"</b>!<br>Senha: "+senha.value+"<br>";
 }// criação da função
+function limpar(){
+    login.value="";
+    senha.value="";
+    outrasaida.innerHTML="";
 
+}
 boas_vindas("Manuele");//chamada da função, execução da função
 boas_vindas("Anacleto");
